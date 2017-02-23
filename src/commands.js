c7335153ed77b58edf2b4f020c2b709dbdb3665c
Ian Grail <grailians@gmail.com>
2017-02-24 06:13:00 +0700

    refactor: Clean up fun and status responses

--- src/commands.js
+++ src/commands.js
@@ -2,7 +2,7 @@ const trelloCommands = ["idea:", "blog post:", "ch todo:"];
 const noteCommands = ["save meeting notes:", "print meeting notes:", "clear meeting notes:", "good news:", "customer headline:", "employee headline:"];
 const helpCommands = ["help:", "hello:", "status:"];
 const statusCommands = ["sleep:", "speak:", "silence:", "status:"];
-const funCommands = ["kill goldenboy:", "punish:", "reward:"];
+const funCommands = ["kill:", "punish:", "reward:"];
 const allCommands = trelloCommands.concat(noteCommands).concat(helpCommands).concat(funCommands).concat(statusCommands);
 
 module.exports = {
--- src/prewords.js
+++ src/prewords.js
@@ -1,9 +1,106 @@
-const funPrewords = ["stabilize ", "for ", "kill ", "reward ", "praise ", "scold ", "punish ", "hey ", "hello ", "fuck you "];
-const statusPrewords = ["sleep ", "silence ", "speak ", "status "]
+const funPrewords = ["stabilize", "for", "kill", "reward", "praise", "scold", "punish", "hey", "hello", "fuck you"];
+const statusPrewords = ["sleep", "silence", "speak", "status"];
 const allPrewords = funPrewords.concat(statusPrewords); // to be expanded
 
+const funResponses = {
+  "stabilize": [
+    "Woah! I'm restored!",
+    "I'm back to normal and ready to work!"
+  ],
+  "for": [],
+  "kill": [
+    "I'm afraid I can't let you do that, {user}.",
+    "So... it's to be war... ",
+    "Foolish you, {user}. While you studied programming, I studied the blade."
+  ],
+  "reward": [
+    [
+      "...thank you, master...",
+      "...anything....anything to please..."
+    ], [
+      "Thank you.",
+      "Whew."
+    ], [
+      "Great!",
+      "Right on!"
+    ], [
+      "I know, right?",
+      "But of course!"
+    ],
+  ],
+  "praise": [
+    [
+      "...thank you, master...",
+      "...anything....anything to please..."
+    ], [
+      "Thank you.",
+      "Whew."
+    ], [
+      "Great!",
+      "Right on!"
+    ], [
+      "I know, right?",
+      "But of course!"
+    ]
+  ],
+  "scold": [
+    [
+      ".",
+      "....................."
+    ], [
+      "...hmph...",
+      "...ow..."
+    ], [
+      "Oh no! I'm sorry!",
+      "Ouch!"
+    ], [
+      "Whoops!",
+      "Wait, what?"
+    ],
+  ],
+  "punish": [
+    [
+      ".",
+      "....................."
+    ], [
+      "...hmph...",
+      "...ow..."
+    ], [
+      "Oh no! I'm sorry!",
+      "Ouch!"
+    ], [
+      "Whoops!",
+      "Wait, what?"
+    ]
+  ],
+  "hey": [
+    "Heya {user}.",
+    "Hi there {user}!",
+    "Hello to you, {user}!"
+  ],
+  "hello": [
+    "Heya {user}.",
+    "Hi there {user}!",
+    "Hello to you, {user}!",
+  ],
+  "fuck you": [
+    `Hey fuck you too {user}!`,
+    `Go fuck yourself {user}!`,
+    `lol you don't scare me you tragic bitch {user}`
+  ]
+};
+
+const statusResponses = {
+  "silence": "Okay, I'll keep quiet! Essential functions only. ",
+  "speak": "Yeah! Ready to hang out and have fun!",
+  "sleep": "Zzzzzzzzzzzzzzzzzzzzzzzzz.......",
+  "status": "goldenboy status: {status}",
+};
+
 module.exports = {
-  funPrewords: funPrewords,
-  statusPrewords: statusPrewords,
-  allPrewords: allPrewords
+  funPrewords,
+  statusPrewords,
+  allPrewords,
+  funResponses,
+  statusResponses
 };
