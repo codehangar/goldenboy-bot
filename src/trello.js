65c8be981d4e46e1ce8e4daca4f83d1f396bbf69
Ian Grail <grailians@gmail.com>
2017-02-24 10:27:52 +0700

    fix(trello): Update trello meeting notes commands to use description instead of comments

--- src/trello.js
+++ src/trello.js
@@ -1,41 +1,92 @@
 const simplyTrello = require('simply-trello');
+const Trello = require("node-trello");
+const trello = new Trello(process.env.TRELLO_KEY, process.env.TRELLO_TOKEN);
 const trelloKey = process.env.TRELLO_KEY;
 const trelloToken = process.env.TRELLO_TOKEN;
 const bot = require('./bot');
 
 let meetingNotesMaster = [];
 
-function notesToString(meetingNotesMaster) {
-  let notesString = "";
-  for (const i in meetingNotesMaster) {
-    console.log(i);
-    console.log(meetingNotesMaster[i]);
-    console.log(meetingNotesMaster[i].tag);
-    notesString += (meetingNotesMaster[i].tag + " ");
-    notesString += (meetingNotesMaster[i].text + " ");
-    notesString += ("\(added by " + meetingNotesMaster[i].user + "\)\n")
-
+function noteToString(note) {
+  let text = note.text;
+  if (text.substring(0, 1) == ' ') {
+    text = text.substring(1);
   }
-  return notesString
+  return `- ${text} (added by ${note.user})`;
+}
+
+function getMeetingNotes(boardTitle, listTitle, cardTitle) {
+  return new Promise((resolve, reject) => {
+    trello.get('/1/members/me/boards', (err, data) => {
+      if (err) reject(err);
+      const board = data.find(board => board.name === boardTitle);
+      trello.get('/1/boards/' + board.id + '/lists', (err, data) => {
+        if (err) reject(err);
+        const list = data.find(list => {
+          return list.name === listTitle
+        });
+        trello.get('/1/lists/' + list.id + '/cards', (err, data) => {
+          if (err) reject(err);
+          const card = data.find(card => card.name === cardTitle);
+          resolve(card);
+        });
+      });
+    });
+  });
+}
+
+function printMeetingNotes(messageChannel, boardTitle, listTitle, cardTitle) {
+  getMeetingNotes(boardTitle, listTitle, cardTitle).then(card => {
+    bot.sendMessage(messageChannel, card.desc);
+  });
+}
+
+function parseMeetingNoteItems(desc, title) {
+  const sectionRegex = new RegExp(title + '\n(-){3,}\n(- .*(\n)?)*', 'g');
+  const section = desc.match(sectionRegex) || [''];
+  return section[0].match(/- .*/g) || [];
+}
+
+function addMeetingNote(boardTitle, listTitle, cardTitle, meetingNote) {
+  getMeetingNotes(boardTitle, listTitle, cardTitle).then(card => {
+    const goodNews = parseMeetingNoteItems(card.desc, 'Good News');
+    const empHeadline = parseMeetingNoteItems(card.desc, 'Employee Headlines');
+    const custHeadline = parseMeetingNoteItems(card.desc, 'Customer Headlines');
+
+    if (meetingNote.text) {
+      if (meetingNote.tag === 'good news:') {
+        goodNews.push(noteToString(meetingNote))
+      } else if (meetingNote.tag === 'employee headline:') {
+        empHeadline.push(noteToString(meetingNote))
+      } else if (meetingNote.tag === 'customer headline:') {
+        custHeadline.push(noteToString(meetingNote))
+      }
+    }
+
+    const cardDesc = `
+Good News
+------------
+${goodNews.join('\n')}
+
+Employee Headlines
+------------
+${empHeadline.join('\n')}
+
+Customer Headlines
+------------
+${custHeadline.join('\n')}
+`;
+    console.log(cardDesc);
+    updateTrello(meetingNote.channel, listTitle, cardTitle, {cardDesc});
+  });
 }
 
 function updateMeetingNotes(command, messageText, messageChannel, messageUser) {
-  if (command == "clear meeting notes:") {
-    console.log("clearing meeting notes");
-    console.log(messageChannel);
-    bot.sendMessage(messageChannel, "Clearing meeting notes! They're all gone!");
-    meetingNotesMaster = [];
-  } else if (command == "save meeting notes:") {
-    bot.sendMessage(messageChannel, "Saving meeting notes to Trello!");
-    console.log("saving meeting notes");
-    const cardTitle = "Meeting Notes";
-    const cardComment = notesToString(meetingNotesMaster);
-    const cardList = getCardListFromCommand(command);
-    updateTrello(messageChannel, cardList, cardTitle, cardComment);
-  } else if (command == "print meeting notes:") {
-    const notesString = notesToString(meetingNotesMaster);
-    bot.sendMessage(messageChannel, notesString);
-    console.log(notesString);
+  const boardTitle = 'Code Hangar General';
+  const cardTitle = 'Meeting Notes';
+  const listTitle = getCardListFromCommand(command);
+  if (command == 'print meeting notes:') {
+    printMeetingNotes(messageChannel, boardTitle, listTitle, cardTitle);
   } else {
     bot.sendMessage(messageChannel, "I'll add that to the notes!");
     const writeText = messageText.split(':')[1];
@@ -46,25 +97,26 @@ function updateMeetingNotes(command, messageText, messageChannel, messageUser) {
       channel: messageChannel
     };
     meetingNotesMaster.push(meetingNote);
+    addMeetingNote(boardTitle, listTitle, cardTitle, meetingNote);
   }
   const writeMessage = messageText.split(command)[1];
 }
 
 function getCardListFromCommand(command) {
   const listDict = {
+    "print meeting notes:": "This Week",
     "save meeting notes:": "This Week",
     "good news:": "This Week",
     "customer headline:": "This Week",
     "employee headline:": "This Week",
     "idea:": "Open",
-    "blog post:": "Blog Article Ideas",
-    "ch todo:": "Open"
+    "ch todo:": "Open",
+    "blog post:": "Blog Article Ideas"
   };
-  const cardList = listDict[command];
-  return cardList;
+  return listDict[command];
 }
 
-function updateTrello(messageChannel, cardList, cardTitle, cardComment) {
+function updateTrello(messageChannel, cardList, cardTitle, content) {
 
   let cardBoard;
   if (cardList == "Blog Article Ideas") {
@@ -90,14 +142,12 @@ function updateTrello(messageChannel, cardList, cardTitle, cardComment) {
       list: cardList,
       card: cardTitle
     },
-    content: {
-      cardComment: cardComment,
-    }
+    content: content
   });
 }
 
 module.exports = {
-  updateMeetingNotes: updateMeetingNotes,
-  getCardListFromCommand: getCardListFromCommand,
-  updateTrello: updateTrello
+  updateMeetingNotes,
+  getCardListFromCommand,
+  updateTrello
 };
