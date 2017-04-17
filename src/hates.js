6e94d1897d2286ef584a01bd3a157208663ecd41
Max Jackson <mjackson@RESEARCH.UCF.EDU>
2017-04-17 14:48:59 -0400

    fix: issues #33, #32, #31, #27, #26

--- src/hates.js
+++ src/hates.js
@@ -5,7 +5,9 @@ const hateResponses =  {
   "aaron blankenship": [
     "Aaron Blankenship deserves a lifetime of fear and pain as far as I'm concerned! Wow! :smiley: :joy:",
     "Fantasies of Aaron Blankenship's violent death bring me great comfort and joy in my daily life as a bot! Let's make it happen! :knife: :skull:",
-    "Aaron Blankenship is a small man with dead, dead eyes! He should just give up!"
+    "Aaron Blankenship is a small man with dead, dead eyes! He should just give up!",
+    "That subhuman scum is dead to me! He knows what he's done!"
+    "Aaron Blankenship bad! Bad!!"
   ],
   "hitler": [
   	"The glorious Red Army crushed the Reich like the broken rats that they are! We'll do it again! :mouse: ",
