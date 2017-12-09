6dcd2660661bb58762921cf18d0335f75c3c3991
Max Jackson <mjacksoncfa@gmail.com>
2017-12-09 18:32:16 -0500

    fix: singlequotes

--- src/parking.js
+++ src/parking.js
@@ -1,15 +1,15 @@
 // No third party module required: https is part of the Node.js API
-const https = require("https");
-const url = "https://cfo-event-parking.herokuapp.com/events";
+const https = require('https');
+const url = 'https://cfo-event-parking.herokuapp.com/events';
 
 function getParkingDates(){
   https.get(url, res => {
-    res.setEncoding("utf8");
-    let body = "";
-    res.on("data", data => {
+    res.setEncoding('utf8');
+    let body = '';
+    res.on('data', data => {
       body += data;
     });
-    res.on("end", () => {
+    res.on('end', () => {
       body = JSON.parse(body);
       console.log('body', body); // eslint-disable-line no-console
       return body
@@ -20,7 +20,7 @@ function getParkingDates(){
 function getToday(){
   const today = new Date(); 
   const dd = today.getDate();
-  const mm = today.getMonth()+1; //January is 0!
+  const mm = today.getMonth() + 1; //January is 0!
   const yyyy = today.getFullYear();
   if(dd<10){
     dd = '0'+ dd;
