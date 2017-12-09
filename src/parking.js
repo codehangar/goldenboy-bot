7d44b28a5e37621b55a9946ecc4cb3891e728b24
Max Jackson <mjacksoncfa@gmail.com>
2017-12-09 18:39:55 -0500

    fix: whitespace v2

--- src/parking.js
+++ src/parking.js
@@ -3,39 +3,39 @@ const https = require('https');
 const url = 'https://cfo-event-parking.herokuapp.com/events';
 
 function getParkingDates() {
-  https.get(url, res => {
-    res.setEncoding('utf8');
-    let body = '';
-    res.on('data', data => {
-      body += data;
+    https.get(url, res => {
+        res.setEncoding('utf8');
+        let body = '';
+        res.on('data', data => {
+            body += data;
+        });
+        res.on('end', () => {
+            body = JSON.parse(body);
+            console.log('body', body); // eslint-disable-line no-console
+            return body;
+        });
     });
-    res.on('end', () => {
-      body = JSON.parse(body);
-      console.log('body', body); // eslint-disable-line no-console
-      return body;
-    });
-  });
 }
 
 function getToday() {
-  const today = new Date(); 
-  const dd = today.getDate();
-  const mm = today.getMonth() + 1; //January is 0!
-  const yyyy = today.getFullYear();
-  if(dd<10){
-    dd = '0'+ dd;
-  } 
-  if(mm<10){
-    mm = '0'+ mm;
-  } 
-  const today_string = yyyy + '-' + mm + '-' + dd;
-  return today_string;
-}
+    const today = new Date(); 
+    const dd = today.getDate();
+    const mm = today.getMonth() + 1; //January is 0!
+    const yyyy = today.getFullYear();
+    if(dd<10){
+        dd = '0'+ dd;
+    } 
+    if(mm<10){
+        mm = '0'+ mm;
+    } 
+    const today_string = yyyy + '-' + mm + '-' + dd;
+    return today_string;
+  }
 
 function thereIsParkingToday() {
-  return getParkingDates().includes(getToday())
+    return getParkingDates().includes(getToday())
 }
 
 module.exports = {
-  getParkingDates, getToday, thereIsParkingToday
+    getParkingDates, getToday, thereIsParkingToday
 };
